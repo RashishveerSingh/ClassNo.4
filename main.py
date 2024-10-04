0ff3f9677b155d3e8bb3104680538ad1f2dba0bf
RashishveerSingh <ashstormsaber@gmail.com>
2024-10-04 21:15:12 +0800

    initial

---
+++ main.py
@@ -0,0 +1,34 @@
+# activity 1 find the average
+
+i = 1000
+o = 400
+n = 300
+e = 600
+g = 44
+
+sum = i+o+n+e+g
+
+avg = sum/5
+print("\nThe average value is",avg)
+
+# activity 2 count the notes
+
+x = int(input("Enter the amount:"))
+z = x//100
+m = (z%100)//50
+q = (z%50)//10
+print("Notes of 100 :",z)
+print("Notes of 50:",m)
+print("Notes of 10:",q)
+
+# activity 3 find the percentage
+astrology = int(input("Enter astology marks:"))
+physics = int(input("Enter physics marks:"))
+biology = int(input("Enter biology marks:"))
+chemistry = int(input("Enter chemistry marks:"))
+
+sum2 =  astrology+physics+biology+chemistry
+
+percentage = (sum2/400)*100
+
+print("The percentage is:",percentage)
